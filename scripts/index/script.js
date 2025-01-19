76da26850b349106e03f41cf1a90b7491ef1c1f2
umi <buttumi0@gmail.com>
2025-01-19 15:03:40 +0530

    login

--- scripts/index/script.js
+++ scripts/index/script.js
@@ -1,5 +1,53 @@
+const baseUrl=""
 const loginButton=document.querySelector("#loginbutton");
 loginButton.addEventListener("click",(e)=>{
-
-    window.location.href="./home.html"
+    login();
+   // window.location.href="./home.html"
 })
+
+const login=()=>{
+    const loginForm= document.querySelector("#loginform");
+    loginForm.classList.add("was-validated");
+    if(!loginForm.checkValidity())
+    {
+        console.log("canceled")
+        return;
+    }
+    const formData= new FormData(loginForm);
+
+    const empid=formData.get("empid"); 
+    const password=formData.get("password"); 
+    console.log(empid,password);
+    const url=`${baseUrl}/login`
+    const data={
+        empid,
+        password
+    }
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+
+    })
+    .then(response => {
+        
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();  
+      })
+    .then((res)=>
+{
+    console.log(res);
+})
+.catch((res)=>{
+    document.querySelector("#error-message").classList.add("alert-danger")
+    document.querySelector("#error-message").classList.remove("alert-info")
+    document.querySelector("#error-message").textContent="Invalid Empid or password"
+    console.log(res);
+})
+
+
+}
